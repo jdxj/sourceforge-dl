dc181183f77707955a80a513a9cd893b29cfbe72
wangyq <985759262@qq.com>
2024-05-01 16:38:25 +0800

    feat: 下载文件

--- src/sourceforge_downloader.rs
+++ src/sourceforge_downloader.rs
@@ -1,20 +1,20 @@
 use chrono::{DateTime, Utc};
-use rss::Channel;
-use std::cmp::Ordering;
-use std::error::Error;
-use std::path::Path;
-use std::time::Duration;
+use futures_util::StreamExt;
+use http::header::{ACCEPT, ACCEPT_ENCODING};
 use log::{debug, error, info};
+use reqwest::redirect::Policy;
+use rss::Channel;
+use std::{cmp::Ordering, error::Error, fs::File, io::Write, path::Path, time::Duration};
 
 #[derive(Debug)]
-struct File {
+struct FileMetaInfo {
     pub_date: DateTime<Utc>,
     download_url: String,
     md5: String,
     name: String,
 }
 
-impl File {
+impl FileMetaInfo {
     fn new(
         pub_date: &str,
         download_url: &str,
@@ -23,7 +23,7 @@ impl File {
     ) -> Result<Self, Box<dyn Error>> {
         let date_time = DateTime::parse_from_rfc2822(pub_date)?.with_timezone(&Utc);
 
-        Ok(File {
+        Ok(FileMetaInfo {
             pub_date: date_time,
             download_url: download_url.to_string(),
             md5: md5.to_string(),
@@ -32,13 +32,13 @@ impl File {
     }
 }
 
-impl PartialEq for File {
+impl PartialEq for FileMetaInfo {
     fn eq(&self, other: &Self) -> bool {
         self.md5.len() > 0 && self.md5 == other.md5
     }
 }
 
-impl PartialOrd for File {
+impl PartialOrd for FileMetaInfo {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         if self.pub_date.le(&other.pub_date) {
             Some(Ordering::Less)
@@ -63,7 +63,7 @@ impl SourceforgeDownloader {
         }
     }
 
-    async fn get_latest_file(&self) -> Result<File, Box<dyn Error>> {
+    async fn get_latest_file(&self) -> Result<FileMetaInfo, Box<dyn Error>> {
         // 获取 rss 内容
         let req = self.http_client.get(&self.rss_url).build()?;
         let content = self.http_client.execute(req).await?.bytes().await?;
@@ -101,20 +101,40 @@ impl SourceforgeDownloader {
             .ok_or("file name can not to str")?;
 
         debug!("pub_date: {:?}, md5: {:?}, name: {:?}", pub_date, md5, name);
-        error!("pub_date: {:?}, md5: {:?}, name: {:?}", pub_date, md5, name);
 
-        let file = File::new(pub_date, download_url, md5, name)?;
+        let file = FileMetaInfo::new(pub_date, download_url, md5, name)?;
         Ok(file)
     }
 
-    async fn download_file() {}
+    async fn download_file(
+        &self,
+        save_path: &str,
+        download_url: &str,
+    ) -> Result<(), Box<dyn Error>> {
+        // 下载文件
+        let req = self.http_client.get(download_url).build()?;
+        let res = self.http_client.execute(req).await?;
+        let mut stream = res.bytes_stream();
+        // 保存到本地
+        let mut file = File::create(save_path)?;
+        while let Some(item) = stream.next().await {
+            let chunk = item?;
+            file.write_all(&chunk)?;
+        }
+        Ok(())
+    }
 }
 
 fn new_http_client() -> reqwest::Client {
+    let mut header_map = reqwest::header::HeaderMap::new();
+    header_map.insert(ACCEPT, "*/*".parse().unwrap());
+    header_map.insert(ACCEPT_ENCODING, "identity".parse().unwrap());
+
     reqwest::ClientBuilder::new()
         .connect_timeout(Duration::from_secs(10))
         .cookie_store(true)
-        .user_agent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+        .user_agent("Wget/1.21.4")
+        .default_headers(header_map)
         .build()
         .unwrap()
 }
@@ -122,8 +142,10 @@ fn new_http_client() -> reqwest::Client {
 #[cfg(test)]
 mod tests {
     use crate::sourceforge_downloader::SourceforgeDownloader;
+    use std::env;
 
     fn setup() {
+        env::set_var("RUST_LOG", "reqwest=trace");
         env_logger::init()
     }
 
@@ -141,6 +163,26 @@ mod tests {
             Err(e) => {
                 eprintln!("{:?}", e)
             }
+        }
+    }
+
+    #[tokio::test]
+    async fn download_file() {
+        setup();
+
+        let sdl = SourceforgeDownloader::new(
+            "https://sourceforge.net/projects/bettercap.mirror/rss?path=/v2.32.0",
+        );
+        let file_meta_info = sdl.get_latest_file().await.unwrap();
+        let save_path = "/tmp/".to_string() + &file_meta_info.name;
+
+        println!("download_url: {:?}", &file_meta_info.download_url);
+
+        if let Err(e) = sdl
+            .download_file(&save_path, &file_meta_info.download_url)
+            .await
+        {
+            eprintln!("{:?}", e)
         }
     }
 }
